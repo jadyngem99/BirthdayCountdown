d8b158ff56d1e79309d2cddfffd78c4a289da3a0
Jordan Hudgens <jordan@devcamp.com>
2018-07-17 09:56:32 -0600

    styled skews and created and imported separate stylesheetsscss

--- src/components/app.js
+++ src/components/app.js
@@ -4,9 +4,9 @@ export default class App extends Component {
   render() {
     return (
       <div className="grid">
-        <div>Hi</div>
-        <div>Hi</div>
-        <div>Hi</div>
+        <div className="grid__skew-dark-one-box" />
+        <div className="grid__skew-dark-two" />
+        <div className="grid__skew-dark-three" />
         <div className="grid__skew-light-one" />
         <div className="grid__skew-light-two" />
         <div className="grid__skew-light-three-box" />
