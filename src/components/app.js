830df778a5ebfc14e3a830cf3e0993a2bd53b7c1
Jordan Hudgens <jordan@devcamp.com>
2018-07-18 11:15:18 -0600

    style fixes

--- src/components/app.js
+++ src/components/app.js
@@ -36,14 +36,11 @@ export default class App extends Component {
     return (
       <div className="grid">
         <h1 className="grid__title">Birthday Countdown</h1>
-
-        <div className="grid__skew-dark-one-box" />
         <div className="grid__skew-dark-two" />
         <div className="grid__skew-dark-three" />
         <div className="grid__skew-light-one" />
         <div className="grid__skew-light-two" />
         <div className="grid__skew-light-three-box" />
-
         {this.renderItems()}
       </div>
     );
