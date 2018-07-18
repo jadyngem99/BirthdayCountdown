7974081191311ad5be8ec9c8c3af7a8615cc4862
Jordan Hudgens <jordan@devcamp.com>
2018-07-18 08:48:02 -0600

    rendering jsx based on app state

--- src/components/app.js
+++ src/components/app.js
@@ -4,12 +4,25 @@ import Button from "./button";
 import Clock from "./clock";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      active: false
+    };
+  }
+
+  renderItems = function() {
+    if (this.state.active) {
+      return [<Clock />];
+    } else {
+      return Button("Generate Countdown", () =>
+        this.setState({ active: true })
+      );
+    }
+  }.bind(this);
+
   render() {
-    //   return (
-    //     <div className="">
-    //       <Clock />
-    //     </div>
-    //   );
     return (
       <div className="grid">
         <h1 className="grid__title">Birthday Countdown</h1>
@@ -22,8 +35,7 @@ export default class App extends Component {
         <div className="grid__skew-light-three-box" />
 
         <Picker />
-        {Button(`Generate Countdown`)}
-        <Clock />
+        {this.renderItems()}
       </div>
     );
   }
--- src/components/button.js
+++ src/components/button.js
@@ -1,7 +1,11 @@
 import React from "react";
 
-const Button = title => {
-  return <button className="button">{title}</button>;
+const Button = (title, callback) => {
+  return (
+    <button className="button" onClick={callback}>
+      {title}
+    </button>
+  );
 };
 
 export default Button;
