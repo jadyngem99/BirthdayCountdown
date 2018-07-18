03191edd7d35e898c98bc6da6b54ff9c6795c09a
Jordan Hudgens <jordan@devcamp.com>
2018-07-18 13:31:59 -0600

    fixed timer state problem, and got timer object into clockjs JSX

--- src/components/app.js
+++ src/components/app.js
@@ -12,6 +12,8 @@ export default class App extends Component {
   constructor(props) {
     super(props);
 
+    this.timer = 0;
+
     this.state = {
       active: false,
       startDate: moment(),
@@ -23,7 +25,7 @@ export default class App extends Component {
       }
     };
 
-    this.handleGenerate = this.handleGenerate.bind(this)
+    this.handleGenerate = this.handleGenerate.bind(this);
   }
 
   handleChange = function(date) {
@@ -40,47 +42,47 @@ export default class App extends Component {
     var countDownDate = this.state.startDate.toDate().getTime();
 
     // Update the count down every 1 second
-    var x = setInterval(function() {
-      // Get todays date and time
-      var now = new Date().getTime();
+    this.timer = setInterval(
+      function() {
+        // Get todays date and time
+        var now = new Date().getTime();
 
-      // Find the distance between now an the count down date
-      var distance = countDownDate - now;
+        // Find the distance between now an the count down date
+        var distance = countDownDate - now;
 
-      // Time calculations for days, hours, minutes and seconds
-      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      var hours = Math.floor(
-        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-      );
-      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor(
+          (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        );
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-      // Output the result in an element with id="demo"
-      const time = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-     
-         const timeRemaining = {
+        // Output the result in an element with id="demo"
+        const time =
+          days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+        const timeRemaining = {
           days,
           hours,
           minutes,
           seconds
+        };
+        this.setState({ timeRemaining });
+
+        // If the count down is over, write some text
+        if (distance < 0) {
+          clearInterval(this.timer);
+          // document.getElementById("demo").innerHTML = "EXPIRED";
         }
-        this.setState({ active: true });
-      });
-
-      console.log(this.setState.timeRemaining);
-
-      // If the count down is over, write some text
-      if (distance < 0) {
-        clearInterval(x);
-        // document.getElementById("demo").innerHTML = "EXPIRED";
-      }
-    }, 1000);
+      }.bind(this),
+      1000
+    );
   }.bind(this);
 
   renderItems = function() {
     if (this.state.active) {
       return [
-        <Clock />,
+        <Clock timeRemaining={this.state.timeRemaining} />,
         ChangeDate("Change Date", () => this.setState({ active: false })),
         LargeText("04/03"),
         <label className="grid__remaining">
